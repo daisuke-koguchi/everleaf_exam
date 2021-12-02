c41ea6919b3c65d3ae3060fd696fabedb552b55c
Daisuke-Koguchi <ds05080906@gmail.com>
2021-12-02 07:43:56 +0000

    Task機能:editアクションとdestroyアクションを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i(show edit update destroy)
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
+  def show  
   end
 
   def new
@@ -22,9 +22,25 @@ class TasksController < ApplicationController
   
   def edit
   end
+  
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path,notice:"タスク「#{@task.name}」を編集しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"タスク「#{@task.name}」を削除しました。"
+  end
 
   private
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
