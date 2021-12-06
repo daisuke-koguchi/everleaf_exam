7f9892e4eb4d6635385257633f77ba5864a9c946
Daisuke-Koguchi <ds05080906@gmail.com>
2021-12-06 21:41:08 +0000

    add task_spec.rbにTask機能の統合テストコードを記入

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -14,9 +14,18 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, name: 'task') 
+        FactoryBot.create(:task, name: 'task')
         visit tasks_path
-        expect(page).not_to have_content 'task'
+        expect(page).to have_content 'task'
+      end
+    end
+  end
+  describe '詳細表示機能' do
+    context '任意のタスク詳細画面に遷移した場合' do 
+      it '該当タスクの内容が表示される'do 
+        @task = FactoryBot.create(:task, name:'task',description:'task')
+        visit task_path(@task)
+        expect(page).to have_content 'task'
       end
     end
   end
