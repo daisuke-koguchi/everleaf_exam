f3ed7e8418fe3f19461850b8068824122ad9f09d
Daisuke-Koguchi <ds05080906@gmail.com>
2021-12-08 23:32:59 +0000

    add index.html.erb、new.html.erb、show.html.erbに終了期限を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -38,7 +38,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :deadline)
   end
   def set_task
     @task = Task.find(params[:id])
