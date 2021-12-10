0e9dc33a52e5415655470bf17d98a54bbd2a5971
Daisuke-Koguchi <ds05080906@gmail.com>
2021-12-10 15:49:11 +0000

    add タスクステータス(未着手・着手中・完了)作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true 
   validates :deadline, presence: true
+  enum status: {'未着手': 0, '着手中': 1, '完了': 2}
 end
