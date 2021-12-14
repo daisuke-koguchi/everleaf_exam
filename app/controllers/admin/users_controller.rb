d674599bf1ac1afb4db2102313a02187548dac3d
Daisuke-Koguchi <ds05080906@gmail.com>
2021-12-14 13:11:58 +0000

    add Adminコントローラーのindexアクション修正し、N+1問題の回避に挑戦する。

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
   skip_before_action :login_required
   def index
-    @users = User.all
+    @users = User.select(:id,:name,:email,:created_at,:updated_at,:admin).includes(:tasks)
   end
 
   def new
